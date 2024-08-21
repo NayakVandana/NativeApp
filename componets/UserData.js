bf714c924062abd86d0f1c8c803bd2e75b579156
Vandana Nayak <vandunayak4000@gmail.com>
2024-08-21 20:52:11 +0530

    components.

---
+++ componets/UserData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+const UserData = () => {
+
+  return(
+    <View>
+      <Text style={{fontSize: 20}}>Name : Vandana</Text>
+    </View>
+  )
+}
+
+export default UserData;
