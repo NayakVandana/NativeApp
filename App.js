fb577b248443edb4b72e64f1937f2a3f64048c19
Vandana Nayak <vandunayak4000@gmail.com>
2024-08-25 21:44:25 +0530

    Component in Loop with Flatlist

--- App.js
+++ App.js
@@ -1,54 +1,47 @@
 import React, { useState } from 'react';
 import {Text, View,StyleSheet,TextInput, Button, FlatList, ScrollView} from 'react-native';
+import UserData from './components/UserData';
 const App = () => {
   <Text style={styles.item}>Sam</Text>
   const users =[
     {
       id:1,
       name:"Test",
+      email:"test@email.com"
     },
     {
       id:2,
       name:"Test2",
+      email:"test2@email.com"
     },
     {
       id:3,
       name:"Test3",
+      email:"test3@email.com"
     },
     {
       id:4,
       name:"Test4",
+      email:"test4@email.com"
     },
     {
       id:5,
       name:"Test5",
+      email:"test5@email.com"
     }
   ]
   
   return (
     <View>
-      <Text style={{fontSize: 31}}>Grid with Dynamic Date</Text>
-      <View style={{flex:1,flexDirection:'row',flexWrap:'wrap'}}>
-        {
-          users.map((item)=><Text style={styles.item}>{item.name}</Text>)
-        }      
-      </View>
+      <Text style={{fontSize: 31}}>Component in Loop with Flatlist</Text>
+      <FlatList
+      data={users}
+      renderItem={({item})=><UserData item={item}/>}
+      
+      />
              
     </View>
   );
 };
 
-const styles =StyleSheet.create({
-  item:{
-    fontSize:25,
-    backgroundColor:'blue',
-    color:'#fff',
-    margin:5,
-    padding:5,
-    width:120,
-    height:120,
-    textAlignVertical:'center',
-    textAlign:'center',
-  }
-})
 export default App;
