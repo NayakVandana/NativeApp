6323a78726b8ff74a053255f90b6fce3ac8a3089
Vandana Nayak <vandunayak4000@gmail.com>
2024-08-27 21:46:03 +0530

    useEffect hook

--- App.js
+++ App.js
@@ -1,29 +1,20 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import {Text, View,StyleSheet,TextInput, Button, FlatList, ScrollView, SectionList} from 'react-native';
-import Student from './components/Student';
+import UserData from './components/UserData';
 
-class App extends Component{
-
-   constructor(){
-    this.state ={
-      name:"Test",
-    }
-   }
-
-   updateName(val){
-    this.setState({name:val})
-   }
-  render(){
+const App = () => {
+  const [count,setCount] = useState(0);
+  useEffect(()=>{
+    console.warn("Hello");
+  },[])
+  
     return(
 
       <View>
-        <Text style={{fontSize:30,color:'red'}}>{this.state.name}</Text>
-        <TextInput  placeholder='Enter your name' 
-        onChangeText={(text)=> this.updateName(text)}/>
-        <Button title="Press Me"/>
-        <Student name={this.state}/>
+        <Text style={{fontSize}}>Life Cycle with Use effect {count}</Text>
+        <Button title='UpdateCount' onPress={()=>setCount(2)}/>
       </View>
     )
-  }
+  
 }
 export default App;
