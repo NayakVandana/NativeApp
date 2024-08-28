c39cc75d9407e803f5535459a90d79530cc7fbce
Vandana Nayak <vandunayak4000@gmail.com>
2024-08-28 19:46:47 +0530

    useEffect hook 2

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import React, { Component, useEffect, useState } from 'react';
-import {Text, View,StyleSheet,TextInput, Button, FlatList, ScrollView, SectionList} from 'react-native';
+import React, {Component, useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  FlatList,
+  ScrollView,
+  SectionList,
+} from 'react-native';
 import UserData from './components/UserData';
 
 const App = () => {
-  const [count,setCount] = useState(0);
-  useEffect(()=>{
-    console.warn("Hello");
-  },[])
-  
-    return(
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(100);
 
-      <View>
-        <Text style={{fontSize}}>Life Cycle with Use effect {count}</Text>
-        <Button title='UpdateCount' onPress={()=>setCount(2)}/>
-      </View>
-    )
-  
-}
+  // useEffect(() => {
+  //   console.warn('do some animation');
+  // }, [count]);
+
+  // useEffect(() => {
+  //   console.warn('call some api here');
+  // }, [data]);
+
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>
+        {data} Life Cycle with Use effect {count}
+      </Text>
+      <Button title="Update Count" onPress={() => setCount(count + 1)} />
+      <Button title="Update data" onPress={() => setData(data + 1)} />
+      <User info={{data, count}} />
+    </View>
+  );
+};
+
+const User = () => {
+  // console.warn(props.info);
+
+  useEffect(() => {
+    console.warn('run this code when prop is update');
+  }, [props.info.data]);
+
+  useEffect(() => {
+    console.warn('run this code when prop is update');
+  }, [props.info.count]);
+
+  return (
+    <View>
+      <Text style={{fontSize: 30, color: 'orange'}}>
+        data : {props.info.data}
+      </Text>
+      <Text style={{fontSize: 30, color: 'orange'}}>
+        data : {props.info.count}
+      </Text>
+    </View>
+  );
+};
 export default App;
