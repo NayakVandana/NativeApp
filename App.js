89a3f0916ac063596538ef13288c4675d7fdc66f
Vandana Nayak <vandunayak4000@gmail.com>
2024-08-29 21:06:45 +0530

    Responsive Layout with Flexbox

--- App.js
+++ App.js
@@ -1,34 +1,54 @@
 import React, {Component, useEffect, useState} from 'react';
 import {
-  Text,
-  View,
   StyleSheet,
-  TextInput,
-  Button,
-  FlatList,
-  ScrollView,
-  SectionList,
+  View,
 } from 'react-native';
-import UserData from './components/UserData';
+
 
 const App = () => {
-  const [show, setShow] = useState(false);
+ 
    return (
-    <View>
-        <Text style={{fontSize: 30}}>Show/Hide</Text>  
-        <Button title='Toggle component' onPress={()=>setShow(!show)}/>        
-          {
-            show ?<User/> : null
-          }       
+    <View style={styles.main}>
+        <View style={styles.box1}>
+          <View style={styles.InnerBox1}></View>   
+          <View style={styles.InnerBox2}></View>   
+          <View style={styles.InnerBox3}></View>   
+        </View>    
+        <View style={styles.box2}></View>   
+        <View style={styles.box3}></View>   
     </View>
   );
 };
 
-const User = () => {
-  return (
-    <View>
-      <Text style={{fontSize: 30, color: 'green'}}>User Component</Text>    
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+ main:{
+  flex:1,
+ },
+ box1:{
+  flex:2,backgroundColor:'red',
+  flexDirection:'row',
+ },
+ box2:{
+  flex:2,backgroundColor:'green'
+ },
+ box3:{
+  flex:2,backgroundColor:'blue'
+ },
+ InnerBox1:{
+  flex:1,
+  backgroundColor:'skyblue',
+  margin:10,
+ },
+ InnerBox2:{
+  flex:1,
+  backgroundColor:'blue',
+  margin:10,
+ },
+ InnerBox3:{
+  flex:1,
+  backgroundColor:'orange',
+  margin:10,
+ }
+})
+
 export default App;
