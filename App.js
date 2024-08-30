4e1c05880ae2cf7b427eb54883c254496dd5c3f1
Vandana Nayak <vandunayak4000@gmail.com>
2024-08-30 20:54:41 +0530

    Activity Indicator Loader

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component, useEffect, useState} from 'react';
 import {
+  ActivityIndicator,
   Button,
   StyleSheet,
   Text,
@@ -9,43 +10,23 @@ import {
 } from 'react-native';
 
 const App = () => {
-  const [selectedRadion, SetSelectedRadio] = useState(1);
+  const [show, SetShow] = useState(flase);
 
-  const skills = [
-    {
-      id: 1,
-      name: 'JAVA',
-    },
-    {
-      id: 1,
-      name: 'PHP',
-    },
-    {
-      id: 1,
-      name: 'Node',
-    },
-    {
-      id: 1,
-      name: 'SQL',
-    },
-  ];
+  displayLoader = () => {
+    SetShow(true);
+
+    setTimeout(()=>{
+      SetShow(false)
+    },3000);
+  };
 
   return (
     <View style={styles.main}>
-      {skills.map((item,index) => (
-        <TouchableOpacity 
-        key={index}
-        onPress={() => SetSelectedRadio(item.id)}>
-          <View style={styles.radioWrapper}>
-            <View style={styles.radio}>
-              {selectedRadion == item.id? (
-                <View style={styles.radioBg}></View>
-              ) : null}
-            </View>
-            <Text style={styles.radioText}>{item.name}</Text>
-          </View>
-        </TouchableOpacity>
-      ))}
+      {/* <ActivityIndicator size={100} color="green" animating={show} /> */}
+      {
+        show ?  <ActivityIndicator size="large" color="green" animating={show}  /> : null
+      }
+      <Button title="show loader" onPress={displayLoader} />
     </View>
   );
 };
@@ -55,29 +36,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  radioText: {
-    fontSize: 20,
-    color: 'skyblue',
-  },
-  radio: {
-    height: 40,
-    width: 40,
-    borderColor: 'skyblue',
-    borderWidth: 2,
-    borderRadius: 20,
-    margin: 10,
-  },
-  radioWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  radioBg: {
-    borderColor: 'skyblue',
-    height: 28,
-    width: 28,
-    borderRadius: 20,
-    margin: 4,
   },
 });
 
