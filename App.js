029ef03af23a71b2cd0263d680659cb235cb9a6a
Vandana Nayak <vandunayak4000@gmail.com>
2024-08-30 21:16:03 +0530

    Modal

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component, useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   Button,
+  Modal,
   StyleSheet,
   Text,
   TouchableHighlight,
@@ -10,33 +11,54 @@ import {
 } from 'react-native';
 
 const App = () => {
-  const [show, SetShow] = useState(flase);
-
-  displayLoader = () => {
-    SetShow(true);
-
-    setTimeout(()=>{
-      SetShow(false)
-    },3000);
-  };
+  const [show, SetShow] = useState(false);
 
   return (
     <View style={styles.main}>
-      {/* <ActivityIndicator size={100} color="green" animating={show} /> */}
-      {
-        show ?  <ActivityIndicator size="large" color="green" animating={show}  /> : null
-      }
-      <Button title="show loader" onPress={displayLoader} />
+      <Modal 
+      transparent={true}
+      visible={showModal}
+      animationType='slide'
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello</Text>
+            <Button title='Close Modal' onPress={()=>setShowModal(false)}/>
+          </View>
+        </View>
+      </Modal>
+      <View style={styles.buttonView}>
+        <Button title="Open Modal" onPress={()=>setShowModal(true)}/>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   main: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,    
   },
+  buttonView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  centeredView:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  modalView:{
+    backgroundColor:'#fff',
+    padding:30,
+    borderRadius:20,
+    shadowColor:'black',
+    elevation:5,
+  },
+  modalText:{
+    fontSize:30,
+    margin:20,
+
+  }
 });
 
 export default App;
