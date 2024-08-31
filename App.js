501e431373f8c165f9543694c18f0127ee800b26
Vandana Nayak <vandana.nayak@binarysphere.com>
2024-08-31 11:47:18 +0530

    changes update

--- App.js
+++ App.js
@@ -1,29 +1,58 @@
-import React, { Component, useState } from 'react';
-import {Text, View,StyleSheet,TextInput, Button, FlatList, ScrollView, SectionList} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
 import Student from './components/Student';
 
-class App extends Component{
+class App extends Component {
+  constructor(props) {
+    super(props); // Call super to initialize the component
+    this.state = {
+      name: 'Test',
+    };
 
-   constructor(){
-    this.state ={
-      name:"Test",
-    }
-   }
+    // Bind the updateName method to this component
+    this.updateName = this.updateName.bind(this);
+  }
 
-   updateName(val){
-    this.setState({name:val})
-   }
-  render(){
-    return(
+  updateName(val) {
+    this.setState({ name: val });
+  }
 
-      <View>
-        <Text style={{fontSize:30,color:'red'}}>{this.state.name}</Text>
-        <TextInput  placeholder='Enter your name' 
-        onChangeText={(text)=> this.updateName(text)}/>
-        <Button title="Press Me"/>
-        <Student name={this.state}/>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{this.state.name}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          onChangeText={this.updateName}
+        />
+        <Button title="Press Me" onPress={() => alert('Button Pressed')} />
+        <Student name={this.state.name} />
       </View>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 30,
+    color: 'red',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    width: '80%',
+  },
+});
+
 export default App;
