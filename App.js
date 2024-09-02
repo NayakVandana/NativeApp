1a99552da9e2d9887cd712858197544dadeefd5f
Vandana Nayak <vandunayak4000@gmail.com>
2024-09-02 20:32:32 +0530

    Status Bar

--- App.js
+++ App.js
@@ -3,44 +3,35 @@ import {
   Button,
   Modal,
   Pressable,
+  StatusBar,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
 const App = () => {
-  
+  const [hide,setHide]=useState(false);
+  const [barStyle,setBarStyle]=useState("default");
 
   return (
-    <View style={styles.main}>
-       <Pressable
-      //  onPress={()=>console.warn("normal on press")}
-      //  onLongPress={()=>console.warn("long press")}
-       onPressIn={()=>console.warn("press in")}
-       onPressOut={()=>console.warn("press out")}
-       >
-          <Text style={styles.pressableBtn}>Pressable</Text>
-       </Pressable>
+    <View style={styles.container}>
+      <StatusBar
+      backgroundColor="blue"
+      barStyle={barStyle}
+      hidden={hide}
+      />
+      <Button title='Toggle Status Bar' onPress={()=>setHide(!hide)}/>
+      <Button title='Update Style'onPress={()=>setBarStyle("dark-content")}/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  main: {
+  container: {
     flex: 1,  
     justifyContent:'center',
   },
-  pressableBtn:{
-  backgroundColor:'blue',
-  color:'#fff',
-  padding:10,
-  margin:10,
-  borderRadius:10,
-  fontSize:20,
-  textAlign:'center',
-  shadowColor:'#000',
-  elevation:5,
-  } 
+ 
 });
 
 export default App;
