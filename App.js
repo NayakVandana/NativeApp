48502276da17397a607f9bff310c735e0e563a91
Vandana Nayak <vandunayak4000@gmail.com>
2024-09-06 21:14:30 +0530

    Custom Modal

--- App.js
+++ App.js
@@ -9,11 +9,46 @@ import {
   Text,
   View,
 } from 'react-native';
-import {WebView} from 'react-native-webview';
 
 const App = () => {
-  return <WebView source={{url: 'https://reactnative.dev/'}} />;
+  const [show,setShow] = useState(false);
+  return (
+    <View style={styles.conatiner}>
+      {
+        show ?
+        <View style={styles.modal}>
+        <View style={styles.body}>
+          <Text>Some text</Text>
+          <Button title="Open Dialog" onPress={()=>setShow(false)}/>
+        </View>
+      </View> 
+      :null
+      }
+      <Button title="Open Dialog" onPress={()=>setShow(true)}/>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  conatiner: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  body: {
+    backgroundColor: '#fff',
+    height: 300,
+    width: 300,
+    padding: 20,
+    justifyContent: 'flex-end',
+    borderRadius:10,
+  },
+});
 
 export default App;
 //npm i react-native-webview
